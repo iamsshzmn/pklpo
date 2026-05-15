a484c626f83d0426f6f93fde985755937f38fbf1
jorrfur <chacha1337@yandex.com>
2026-05-15 12:07:37 +0400

    feat: BootstrapSummary, merge_bootstrap_results, package __init__

---
+++ src/candles/application/bootstrap/__init__.py
@@ -0,0 +1,14 @@
+from .dto import BootstrapCommand, BootstrapProgress, BootstrapResult
+from .ports import BootstrapStatePort
+from .summary import BootstrapSummary, merge_bootstrap_results
+from .use_cases import RunBootstrapUseCase
+
+__all__ = [
+    "BootstrapCommand",
+    "BootstrapProgress",
+    "BootstrapResult",
+    "BootstrapStatePort",
+    "BootstrapSummary",
+    "RunBootstrapUseCase",
+    "merge_bootstrap_results",
+]
